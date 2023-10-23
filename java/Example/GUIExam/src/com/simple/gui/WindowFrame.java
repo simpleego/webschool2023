0800511821256011428db30a2a2a9b360fcb3e13
Jong Cheon Park <simpleego@gmail.com>
2023-10-23 14:49:54 +0900

    Add files via upload

--- java/Example/GUIExam/src/com/simple/gui/WindowFrame.java
+++ java/Example/GUIExam/src/com/simple/gui/WindowFrame.java
@@ -4,16 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
 
-public class WindowFrame extends JFrame {
+public class WindowFrame extends JFrame implements ActionListener {
     // GUI 멤버
-    private final JButton btnAdd,btnSub,btnMul,btnDiv;
+    private JButton btnAdd,btnSub,btnMul,btnDiv,btnPow;
     private JTextField tf1, tf2, tf3;
     private JLabel label1, label2, label3;
-
     public WindowFrame(){
-
         // 윈도우 초기화
         this.setTitle("계산기");
         this.setSize(100,200);
@@ -33,6 +30,8 @@ public class WindowFrame extends JFrame {
         btnSub = new JButton("-");
         btnMul = new JButton("x");
         btnDiv = new JButton("/");
+        btnPow = new JButton("^");
+
         btnAdd.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -44,62 +43,57 @@ public class WindowFrame extends JFrame {
             }
         });
 
-        btnSub.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String num1_ = tf1.getText();
-                String num2_ = tf2.getText();
-                int result = Integer.parseInt(num1_) -
-                        Integer.parseInt(num2_);
-                tf3.setText(result+""); //String.valueOf(sum));
-            }
-        });
-
-        btnMul.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String num1_ = tf1.getText();
-                String num2_ = tf2.getText();
-                int result = Integer.parseInt(num1_) *
-                        Integer.parseInt(num2_);
-                tf3.setText(result+""); //String.valueOf(sum));
-            }
-        });
-
-        btnDiv.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String num1_ = tf1.getText();
-                String num2_ = tf2.getText();
-                double result = 0;
-                if(Integer.parseInt(num2_) != 0){
-                    result = Double.parseDouble(num1_) /
-                          Double.parseDouble(num2_);
-                    String result_ = String.format("%.2f", result);
-                    tf3.setText(result_); //String.valueOf(sum));
-                    return;
-                }
-                // 메시지 박스 띄우기
-                JOptionPane.showMessageDialog(null, "0으로 나눌 수 없습니다.");
-
-                tf3.setText(result+"으로 나눌 수 없습니다 !!"); //String.valueOf(sum));
-
-            }
-        });
+        btnSub.addActionListener(this);
+        btnMul.addActionListener(this);
+        btnDiv.addActionListener(new MyDivider(this));
+        btnPow.addActionListener(new MyPower());
 
         add(label1);add(tf1);
         add(label2);add(tf2);
         add(label3);add(tf3);
-        add(btnAdd);
-        add(btnSub);
-        add(btnMul);
-        add(btnDiv);
+        add(btnAdd);add(btnSub);add(btnMul);add(btnDiv);add(btnPow);
 
         this.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new WindowFrame();
+    public String getTf1() {
+        return tf1.getText();
+    }
+    public String getTf2() {
+        return tf2.getText();
     }
 
+    public void setTf3(String tf3) {
+        this.tf3.setText(tf3);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int result = 0;
+        int num1 = Integer.parseInt(tf1.getText());
+        int num2 = Integer.parseInt(tf2.getText());
+
+        if(e.getSource() == btnSub){
+            result = num1 - num2;
+        }else {
+            result = num1 * num2;
+        }
+        tf3.setText(result+""); //String.valueOf(sum));
+    }
+
+    // 내부 클래스 :  제곱계산을 처리
+    public class MyPower implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            double num1 = Double.parseDouble(tf1.getText());
+            double num2 = Double.parseDouble(tf2.getText());
+            double result = 0;
+            result = Math.pow(num1,num2);
+            tf3.setText(String.valueOf(result));
+        }
+    }
+
+    public static void main(String[] args) {
+        WindowFrame win = new WindowFrame();
+    }
 }
