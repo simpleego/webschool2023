2222d43f3a803e45ad75bdc17820707693a5c2bb
Jong Cheon Park <simpleego@gmail.com>
2023-12-21 16:22:25 +0900

    Add files via upload

--- Web/VM/src/com/simple/vm/VM.java
+++ Web/VM/src/com/simple/vm/VM.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class VM
@@ -35,22 +36,98 @@ public class VM extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String coin_ = request.getParameter("coin");
-		int coin = 0;	
-		int valance = (int) request.getAttribute("valance");
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
 		
-		if(!coin_.equals("")) {
+		String coin_ = request.getParameter("coin");
+		String change = request.getParameter("change");
+		String coffee = request.getParameter("coffee");
+		String logout = request.getParameter("logout");
+		
+		int coin = 0;	
+		int valance = 0;
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("valance") != null) {
+			valance = (int) session.getAttribute("valance");
+		}		
+		
+		if( coin_ != null ) {
 			coin = Integer.parseInt(coin_);
-			// 밀커커피 판매 처리(300원)
-			valance = coin - 300;
+			// 동전 누적
+			valance += coin;
 		}	
 		
-		request.setAttribute("valance", valance);
+		
+		// 동전 반환
+		if(change != null ) {
+			int returnChange = valance;
+			valance = 0;
+			session.setAttribute("change", returnChange);				
+		}
+		
+		// 커피판매
+		if(coffee != null) {
+			String returnCoffee="";
+			if(coffee.equals("밀크커피")) {
+				valance -= 300;
+				returnCoffee = "밀크커피";
+			}else if(coffee.equals("프림커피")) {
+				valance -= 300;
+				returnCoffee = "프림커피";
+			}else if(coffee.equals("설탕커피")) {
+				valance -= 200;
+				returnCoffee = "설탕커피";
+			}else if(coffee.equals("블랙커피")) {
+				valance -= 200;
+				returnCoffee = "블랙커피";
+			}
+			session.setAttribute("coffee", returnCoffee);	
+		}		
+		
+				
+
+		// check button
+		checkButtons(valance, session);
+				
+		session.setAttribute("valance", valance);
+		
+		// 로그아웃 처리
+		if( logout != null && session != null) {			
+			session.invalidate();
+		}
 		
 		RequestDispatcher dispatcher = 
 				request.getRequestDispatcher("index.jsp");
 		
 		dispatcher.forward(request, response);
+		
+	}
+
+	private void checkButtons(int valance, HttpSession session) {
+		
+		String btnMilk="";
+		String btnCream="";
+		String btnSugar="";
+		String btnBlack="";
+		
+		if ( valance >= 300) {
+			// 모든 버튼 활성화
+			btnMilk="enable";
+			btnCream="enable";
+			btnSugar="enable";
+			btnBlack="enable";
+		}else if(valance >= 200) {
+			// 해당 버튼 활성화
+			btnSugar="enable";
+			btnBlack="enable";
+		}
+		
+		session.setAttribute("btnMilk", btnMilk);
+		session.setAttribute("btnCream", btnCream);
+		session.setAttribute("btnSugar", btnSugar);
+		session.setAttribute("btnBlack", btnBlack);
 		
 	}
 
