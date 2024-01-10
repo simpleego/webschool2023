fe610f2f25ff8ddd3655c2b9491e8ec477ac60ba
Jong Cheon Park <simpleego@gmail.com>
2024-01-10 13:38:32 +0900

    Add files via upload

--- Web/ajax/AjaxPrj/src/com/simple/ajax/JsonServlet3.java
+++ Web/ajax/AjaxPrj/src/com/simple/ajax/JsonServlet3.java
@@ -19,14 +19,14 @@ import com.sun.prism.Texture.WrapMode;
 /**
  * Servlet implementation class JsonServlet1
  */
-@WebServlet("/json2")
-public class JsonServlet2 extends HttpServlet {
+@WebServlet("/json3")
+public class JsonServlet3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public JsonServlet2() {
+    public JsonServlet3() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -70,11 +70,31 @@ public class JsonServlet2 extends HttpServlet {
 		memberInfo.put("nickname","칼치");
 		
 		membersArray.add(memberInfo);
-		
 		totalObject.put("members",membersArray);
 		
+		JSONArray bookArray = new JSONArray();
+		JSONObject bookInfo = new JSONObject();
+		
+		bookInfo.put("title","초보자를 위한 자바 프로그래밍");
+		bookInfo.put("writer","이병승");
+		bookInfo.put("price","30000");
+		bookInfo.put("genre","IT");
+		bookInfo.put("image","http://localhost:8080/AjaxPrj/image/image1.jpg");
+		bookArray.add(bookInfo);
+		
+		bookInfo = new JSONObject();
+		bookInfo.put("title","모두의 파이선");
+		bookInfo.put("writer","이승찬");
+		bookInfo.put("price","12000");
+		bookInfo.put("genre","IT");
+		bookInfo.put("image","http://localhost:8080/AjaxPrj/image/image2.jpg");
+		bookArray.add(bookInfo);
+		
+		totalObject.put("books",bookArray);
+		
 		String jsonInfo = totalObject.toJSONString();
-		System.out.println("members:"+jsonInfo);
+		
+		System.out.println("객체:"+jsonInfo);
 		
 		// 배열객체를 문자열로 변환하여 전송
 		writer.print(jsonInfo);
