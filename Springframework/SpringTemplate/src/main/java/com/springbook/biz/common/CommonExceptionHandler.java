7ca1263145e5ba672555c7fddb670ccea4ce7dd5
Jong Cheon Park <simpleego@gmail.com>
2024-02-02 14:46:05 +0900

    Add files via upload

---
+++ Springframework/SpringTemplate/src/main/java/com/springbook/biz/common/CommonExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.springbook.biz.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice("com.springbook.view")
+public class CommonExceptionHandler {
+	
+	// 예외종류별 처리 메서드
+	@ExceptionHandler(ArithmeticException.class)
+	public ModelAndView handleArithmeticException(Exception e) {		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exeception", e);
+		mav.setViewName("/common/arithmeticError.jsp");		
+		return mav;		
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	public ModelAndView handleNullPointerException(Exception e) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exception", e);
+		mav.setViewName("/common/nullPointerError.jsp");
+		return mav;
+	}	
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleException(Exception e) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exception", e);
+		mav.setViewName("/common/error.jsp");
+		return mav;
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ModelAndView handleIllegalArgumentException(Exception e) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exception", e);
+		mav.setViewName("/common/error.jsp");
+		return mav;
+	}	
+
+}
